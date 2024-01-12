ad1290b08021405e8b49292b9a27f344b4ef4aa1
gomezgleonardob <gomezgleonardob@riseup.net>
2024-01-12 12:10:09 -0500

    [IMP]:Separate_Sections_Documentation_E_Invoices

--- conf.py
+++ conf.py
@@ -126,7 +126,7 @@ html_css_files = [
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'files/img/oventi.png'
+html_logo = 'files/img/odoo_ecuador.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
